6227f8f0972508fbc7c8d0b3e243a6eb5565ecc2
madjin <32600939+madjin@users.noreply.github.com>
2024-02-02 20:06:21 -0500

    add files

---
+++ README.md
@@ -0,0 +1,7 @@
+# MetaFactory Shop
+
+Demo: https://m3org.com/mfshop
+
+3D storefront / linktree made with playcanvas.
+
+Deployable to any web server with GZIP support.  (See https://developer.playcanvas.com/en/user-manual/publishing/web/self-hosting-for-beginners/ )
---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1406164.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 80485185, 80485210, 80485212, 80485223, 80485140, 80484984, 80484944, 80484947, 80485171, 80485267, 80484946, 80485176, 80485326, 85465875, 85490933 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/80485174/1/basis.wasm.js', 'wasmUrl' : 'files/assets/80484967/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/80485268/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/80485214/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/80485038/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/80485225/1/ammo.js', 'preload' : true},
+];
